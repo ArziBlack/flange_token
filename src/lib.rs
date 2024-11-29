3187a385c5b631dbde50a19c79a6f14af6ea15b3
Milton Black <arziblack2@gmail.com>
2024-11-29 21:29:06 +0100

    commit

--- src/lib.rs
+++ src/lib.rs
@@ -5,41 +5,35 @@ pub mod utils;
 
 use borsh::{BorshDeserialize, BorshSerialize};
 use freeze::freeze_wallet;
-use metadata::{process_create_metadata, process_update_metadata};
+// use metadata::{process_create_metadata, process_update_metadata};
 use mpl_token_metadata::state::DataV2;
 use process::{process_buy, process_mint, process_sell};
 use solana_program::{
-    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, program_error::ProgramError,
-    pubkey::Pubkey,
+    account_info::AccountInfo, borsh0_10::try_from_slice_unchecked, entrypoint,
+    entrypoint::ProgramResult, hash::Hash, program_error::ProgramError, pubkey::Pubkey,
 };
 use utils::MetadataInfo;
 
 entrypoint!(process_instruction);
 
-#[derive(Clone, BorshSerialize, BorshDeserialize, PartialEq, Debug)]
+#[derive(BorshSerialize, BorshDeserialize, Default)]
 pub enum FlangeInstruction {
-    Buy {
-        amount: u64,
-    },
-    Sell {
-        amount: u64,
-    },
-    Mint {
-        amount: u64,
-    },
+    Buy { amount: u64 },
+    Sell { amount: u64 },
+    Mint { amount: u64 },
     FreezeWallet,
-    CreateMetaDataV3 {
-        metadata_info: MetadataInfo,
-        seller_fee_basis_points: Option<u16>,
-    },
-    UpdateMetaDataV2 {
-        metadata_key: Pubkey,
-        update_authority_info: &AccountInfo,
-        new_update_authority: Option<Pubkey>,
-        data: Option<DataV2>,
-        primary_sale_happened: Option<bool>,
-        is_mutable: Option<bool>,
-    },
+    // CreateMetaDataV3 {
+    //     metadata_info: MetadataInfo,
+    //     seller_fee_basis_points: Option<u16>,
+    // },
+    // UpdateMetaDataV2 {
+    //     metadata_key: Pubkey,
+    //     update_authority_info: &AccountInfo,
+    //     new_update_authority: Option<Pubkey>,
+    //     data: Option<DataV2>,
+    //     primary_sale_happened: Option<bool>,
+    //     is_mutable: Option<bool>,
+    // },
 }
 
 pub struct ManageMetadataArgs {
@@ -50,9 +44,7 @@ pub struct ManageMetadataArgs {
     pub is_create: bool,
 }
 
-pub fn my_try_from_slice_unchecked<T: borsh::BorshDeserialize>(
-    data: &[u8],
-) -> Result<T, ProgramError> {
+pub fn my_try_from_slice_unchecked(data: &[u8]) -> Result<T, ProgramError> {
     let mut data_mut = data;
     match T::deserialize(&mut data_mut) {
         Ok(result) => Ok(result),
@@ -65,40 +57,41 @@ pub fn process_instruction(
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
-    let instruction: FlangeInstruction = FlangeInstruction::try_from(instruction_data)
-        .map_err(|_| ProgramError::InvalidInstructionData)?;
+    let instruction: FlangeInstruction =
+        FlangeInstruction::try_from_slice_unchecked(instruction_data)
+            .map_err(|_| ProgramError::InvalidInstructionData)?;
 
     match instruction {
         FlangeInstruction::Buy { amount } => process_buy(program_id, accounts, amount),
         FlangeInstruction::Sell { amount } => process_sell(program_id, accounts, amount),
         FlangeInstruction::Mint { amount } => process_mint(program_id, accounts, amount),
         FlangeInstruction::FreezeWallet => freeze_wallet(program_id, accounts),
-        FlangeInstruction::CreateMetaDataV3 {
-            metadata_info,
-            seller_fee_basis_points,
-        } => process_create_metadata(
-            program_id,
-            accounts,
-            metadata_info,
-            creators,
-            seller_fee_basis_points,
-        ),
-        FlangeInstruction::UpdateMetaDataV2 {
-            metadata_key,
-            update_authority_info,
-            new_update_authority,
-            data,
-            primary_sale_happened,
-            is_mutable,
-        } => process_update_metadata(
-            program_id,
-            accounts,
-            metadata_key,
-            update_authority_info,
-            new_update_authority,
-            data,
-            primary_sale_happened,
-            is_mutable,
-        ),
+        // FlangeInstruction::CreateMetaDataV3 {
+        //     metadata_info,
+        //     seller_fee_basis_points,
+        // } => process_create_metadata(
+        //     program_id,
+        //     accounts,
+        //     metadata_info,
+        //     creators,
+        //     seller_fee_basis_points,
+        // ),
+        // FlangeInstruction::UpdateMetaDataV2 {
+        //     metadata_key,
+        //     update_authority_info,
+        //     new_update_authority,
+        //     data,
+        //     primary_sale_happened,
+        //     is_mutable,
+        // } => process_update_metadata(
+        //     program_id,
+        //     accounts,
+        //     metadata_key,
+        //     update_authority_info,
+        //     new_update_authority,
+        //     data,
+        //     primary_sale_happened,
+        //     is_mutable,
+        // ),
     }
 }
--- src/metadata.rs
+++ src/metadata.rs
@@ -1,125 +1,126 @@
-use mpl_token_metadata::{
-    instruction::{create_metadata_accounts_v3, update_metadata_accounts_v2},
-    state::{Creator, DataV2, Metadata},
-    ID,
-};
-use solana_program::{
-    account_info::{next_account_info, AccountInfo},
-    msg,
-    program::{invoke, invoke_signed},
-    program_error::ProgramError,
-    pubkey::Pubkey,
-    system_instruction,
-};
+// use mpl_token_metadata::{
+//     instruction::{create_metadata_accounts_v3, update_metadata_accounts_v2},
+//     state::{Creator, DataV2, Metadata},
+//     ID,
+// };
 
-pub struct MetadataInfo {
-    pub name: String,   // The name of the token (e.g., "MyToken").
-    pub symbol: String, // The symbol of the token (e.g., "MTK").
-    pub uri: String,    // The URI pointing to the metadata JSON.
-}
+// use solana_program::{
+//     account_info::{next_account_info, AccountInfo},
+//     msg,
+//     program::{invoke, invoke_signed},
+//     program_error::ProgramError,
+//     pubkey::Pubkey,
+//     system_instruction,
+// };
 
-pub fn process_create_metadata(
-    program_id: &Pubkey,
-    accounts: &[AccountInfo],
-    metadata_info: MetadataInfo,
-    creators: Option<Vec<Creator>>,
-    seller_fee_basis_points: u16,
-) -> Result<(), ProgramError> {
-    let account_info_iter = &mut accounts.iter();
+// pub struct MetadataInfo {
+//     pub name: String,
+//     pub symbol: String,
+//     pub uri: String,
+// }
 
-    let payer = next_account_info(account_info_iter)?;
-    let mint_account = next_account_info(account_info_iter)?;
-    let metadata_account = next_account_info(account_info_iter)?;
-    let token_program = next_account_info(account_info_iter)?;
-    let mint_authority = next_account_info(account_info_iter)?;
+// pub fn process_create_metadata(
+//     program_id: &Pubkey,
+//     accounts: &[AccountInfo],
+//     metadata_info: MetadataInfo,
+//     creators: Option<Vec<Creator>>,
+//     seller_fee_basis_points: u16,
+// ) -> Result<(), ProgramError> {
+//     let account_info_iter = &mut accounts.iter();
 
-    if !payer.is_signer {
-        return Err(ProgramError::MissingRequiredSignature);
-    }
+//     let payer = next_account_info(account_info_iter)?;
+//     let mint_account = next_account_info(account_info_iter)?;
+//     let metadata_account = next_account_info(account_info_iter)?;
+//     let token_program = next_account_info(account_info_iter)?;
+//     let mint_authority = next_account_info(account_info_iter)?;
 
-    let metadata_account_key = *metadata_account.key;
+//     if !payer.is_signer {
+//         return Err(ProgramError::MissingRequiredSignature);
+//     }
 
-    let data = DataV2 {
-        name: metadata_info.name,
-        symbol: metadata_info.symbol,
-        uri: metadata_info.uri,
-        seller_fee_basis_points,
-        creators,
-        collection: todo!(),
-        uses: todo!(),
-    };
+//     let metadata_account_key = *metadata_account.key;
 
-    let create_metadata_ix = create_metadata_accounts_v3(
-        ID,
-        metadata_account_key,
-        *mint_account.key,
-        *mint_authority.key,
-        *payer.key,
-        *payer.key,
-        data.name,
-        data.symbol,
-        data.uri,
-        data.creators,
-        data.seller_fee_basis_points,
-        true,
-        true,
-        None,
-        None,
-        None,
-    );
+//     let data = DataV2 {
+//         name: metadata_info.name,
+//         symbol: metadata_info.symbol,
+//         uri: metadata_info.uri,
+//         seller_fee_basis_points,
+//         creators,
+//         collection: todo!(),
+//         uses: todo!(),
+//     };
 
-    invoke(
-        &create_metadata_ix,
-        &[
-            payer.clone(),
-            mint_account.clone(),
-            metadata_account.clone(),
-            token_program.clone(),
-        ],
-    )?;
+//     let create_metadata_ix = create_metadata_accounts_v3(
+//         ID,
+//         metadata_account_key,
+//         *mint_account.key,
+//         *mint_authority.key,
+//         *payer.key,
+//         *payer.key,
+//         data.name,
+//         data.symbol,
+//         data.uri,
+//         data.creators,
+//         data.seller_fee_basis_points,
+//         true,
+//         true,
+//         None,
+//         None,
+//         None,
+//     );
 
-    msg!("Metadata account created successfully using v3.");
-    Ok(())
-}
+//     invoke(
+//         &create_metadata_ix,
+//         &[
+//             payer.clone(),
+//             mint_account.clone(),
+//             metadata_account.clone(),
+//             token_program.clone(),
+//         ],
+//     )?;
 
-pub fn process_update_metadata(
-    program_id: &Pubkey,
-    accounts: &[AccountInfo],
-    metadata_key: Pubkey,
-    update_authority_info: &AccountInfo,
-    new_update_authority: Option<Pubkey>,
-    data: Option<DataV2>,
-    primary_sale_happened: Option<bool>,
-    is_mutable: Option<bool>,
-) -> Result<(), ProgramError> {
-    let account_info_iter = &mut accounts.iter();
+//     msg!("Metadata account created successfully using v3.");
+//     Ok(())
+// }
 
-    let metadata_account = next_account_info(account_info_iter)?;
-    let update_authority = next_account_info(account_info_iter)?;
+// pub fn process_update_metadata(
+//     program_id: &Pubkey,
+//     accounts: &[AccountInfo],
+//     metadata_key: Pubkey,
+//     update_authority_info: &AccountInfo,
+//     new_update_authority: Option<Pubkey>,
+//     data: Option<DataV2>,
+//     primary_sale_happened: Option<bool>,
+//     is_mutable: Option<bool>,
+// ) -> Result<(), ProgramError> {
+//     let account_info_iter = &mut accounts.iter();
 
-    if !update_authority.is_signer {
-        return Err(ProgramError::MissingRequiredSignature);
-    }
+//     let metadata_account = next_account_info(account_info_iter)?;
+//     let update_authority = next_account_info(account_info_iter)?;
 
-    if metadata_account.key != &metadata_key {
-        return Err(ProgramError::InvalidAccountData);
-    }
+//     if !update_authority.is_signer {
+//         return Err(ProgramError::MissingRequiredSignature);
+//     }
 
-    let update_metadata_ix = update_metadata_accounts_v2(
-        mpl_token_metadata::ID,
-        *metadata_account.key,
-        *update_authority.key,
-        new_update_authority,
-        data,
-        primary_sale_happened,
-        is_mutable,
-    );
+//     if metadata_account.key != &metadata_key {
+//         return Err(ProgramError::InvalidAccountData);
+//     }
 
-    invoke(
-        &update_metadata_ix,
-        &[metadata_account.clone(), update_authority.clone()],
-    )?;
+//     let update_metadata_ix = update_metadata_accounts_v2(
+//         mpl_token_metadata::ID,
+//         *metadata_account.key,
+//         *update_authority.key,
+//         new_update_authority,
+//         data,
+//         primary_sale_happened,
+//         is_mutable,
+//     );
 
-    msg!("Metadata account updated successfully using update_metadata_accounts_v2.");
-    Ok(())
-}
+//     invoke(
+//         &update_metadata_ix,
+//         &[metadata_account.clone(), update_authority.clone()],
+//     )?;
+
+//     msg!("Metadata account updated successfully using update_metadata_accounts_v2.");
+//     Ok(())
+// }
